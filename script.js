18bfc6f910da0d6faf4f9921f07500a710dcda1b
hallwayRaptor <hallwayraptor@gmail.com>
2021-11-29 18:01:57 -0400

    create grid size input and reset button

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// const gameArea = document.querySelector('.game');
-// const gameBlock = document.createElement('div');
+//init userNumber
+let userNumber;
+//create resizeButton attached to resizeButton HTML element
+const resizeButton = document.getElementById('resizeButton');
+//create gameGrid attached to gameGrid HTML element
+const gameGrid = document.getElementById('gameGrid');
+//check if button is being pressed
+const logButton = function () {
+	console.log(`you pressed the button, your number is ${userNumber}`);
+};
 
-// gameArea.appendChild(gameBlock);
-
+//add blocks to gameGrid
 const addBlocks = function () {
-	const gameArea = document.querySelector('.game');
+	const gameArea = document.getElementById(`gameGrid`);
 	const gameBlock = document.createElement('div');
 	gameBlock.classList.add('block');
 	gameArea.appendChild(gameBlock);
@@ -12,7 +19,29 @@ const addBlocks = function () {
 		gameBlock.classList.add('hovered');
 	});
 };
-
-for (let i = 0; i < 16 ** 2; i++) {
-	addBlocks();
-}
+//set grid rows and columns to match added blocks from userNumber
+const changeGrid = function () {
+	gameGrid.style.setProperty(
+		'grid-template-rows',
+		`repeat(${userNumber}, 1fr)`
+	);
+	gameGrid.style.setProperty(
+		'grid-template-columns',
+		`repeat(${userNumber}, 1fr)`
+	);
+};
+//reset blocks before spawning new ones
+const resetBlocks = function () {
+	while (gameGrid.firstChild) {
+		gameGrid.removeChild(gameGrid.firstChild);
+	}
+};
+//reset button - click to change blocks
+resizeButton.addEventListener('click', function () {
+	userNumber = document.getElementById('userNumber').value;
+	resetBlocks();
+	changeGrid();
+	for (let i = 0; i < userNumber ** 2; i++) {
+		addBlocks();
+	}
+});
